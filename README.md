96b0cd9fff5a8ff96be143afded879ae17504f63
LeoAndJellyfish <yyzjmk0123@163.com>
2024-08-10 22:27:23 +0800

    d

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # AIExamGrader
 ModelGrader 是一个基于大模型的自动评分系统，旨在通过人工智能技术对考试卷和论文进行智能化评估。该系统能够分析和评分学生提交的作业，提供准确、公平的评分结果。支持多种类型的考试题目和评分标准，并且可以根据实际需求进行自定义配置。
+---
+环境准备
+需要modelscope，torch，transformers，streamlit等依赖
+```Shell
+# 查看已安装依赖,包括modelscope，torch，transformers等
+pip list
+# 安装 streamlit
+pip install streamlit==1.24.0
+```
+---
+# 向量模型和源大模型下载
+```Python
+# 向量模型下载
+from modelscope import snapshot_download
+model_dir = snapshot_download("AI-ModelScope/bge-small-zh-v1.5", cache_dir='.')
+# 源大模型下载
+from modelscope import snapshot_download
+model_dir = snapshot_download('IEITYuan/Yuan2-2B-Mars-hf', cache_dir='.')
+```
+
+<!-- 运行
+```Shell
+streamlit run app.py
+```
+---
+使用说明
+- 打开浏览器，输入 http://localhost:8501/ 访问系统
+- 登录系统，输入用户名和密码
+- 上传需要评分的作业文件
+- 选择需要评分的考试类型
+- 选择需要评分的评分标准
+- 点击开始评分按钮，系统开始自动评分
+- 系统会自动生成评分结果，并显示在页面上
+- 点击下载按钮，可以下载评分结果文件
+---
+系统架构
+- 前端：使用streamlit构建的web页面，负责用户交互和显示
+- 后端：使用Flask构建的后端服务，负责接收用户上传的作业文件，并调用模型进行评分 -->
---
+++ code/main.py
@@ -0,0 +1,107 @@
+# 导入所需的库
+from typing import List
+import numpy as np
+
+import torch
+from transformers import AutoModel, AutoTokenizer, AutoModelForCausalLM
+# 定义向量模型类
+class EmbeddingModel:
+    """
+    class for EmbeddingModel
+    """
+
+    def __init__(self, path: str) -> None:
+        self.tokenizer = AutoTokenizer.from_pretrained(path)
+
+        self.model = AutoModel.from_pretrained(path).cuda()
+        print(f'Loading EmbeddingModel from {path}.')
+
+    def get_embeddings(self, texts: List) -> List[float]:
+        """
+        calculate embedding for text list
+        """
+        encoded_input = self.tokenizer(texts, padding=True, truncation=True, return_tensors='pt')
+        encoded_input = {k: v.cuda() for k, v in encoded_input.items()}
+        with torch.no_grad():
+            model_output = self.model(**encoded_input)
+            sentence_embeddings = model_output[0][:, 0]
+        sentence_embeddings = torch.nn.functional.normalize(sentence_embeddings, p=2, dim=1)
+        return sentence_embeddings.tolist()
+print("> Create embedding model...")
+embed_model_path = './AI-ModelScope/bge-small-zh-v1___5'
+embed_model = EmbeddingModel(embed_model_path)
+# 定义向量库索引类
+class VectorStoreIndex:
+    """
+    class for VectorStoreIndex
+    """
+
+    def __init__(self, doecment_path: str, embed_model: EmbeddingModel) -> None:
+        self.documents = []
+        for line in open(doecment_path, 'r', encoding='utf-8'):
+            line = line.strip()
+            self.documents.append(line)
+
+        self.embed_model = embed_model
+        self.vectors = self.embed_model.get_embeddings(self.documents)
+
+        print(f'Loading {len(self.documents)} documents for {doecment_path}.')
+
+    def get_similarity(self, vector1: List[float], vector2: List[float]) -> float:
+        """
+        calculate cosine similarity between two vectors
+        """
+        dot_product = np.dot(vector1, vector2)
+        magnitude = np.linalg.norm(vector1) * np.linalg.norm(vector2)
+        if not magnitude:
+            return 0
+        return dot_product / magnitude
+
+    def query(self, question: str, k: int = 1) -> List[str]:
+        question_vector = self.embed_model.get_embeddings([question])[0]
+        result = np.array([self.get_similarity(question_vector, vector) for vector in self.vectors])
+        return np.array(self.documents)[result.argsort()[-k:][::-1]].tolist() 
+print("> Create index...")
+doecment_path = './knowledge.txt'
+index = VectorStoreIndex(doecment_path, embed_model)
+question = '介绍一下广州大学'
+print('> Question:', question)
+
+context = index.query(question)
+print('> Context:', context)
+# 定义大语言模型类
+class LLM:
+    """
+    class for Yuan2.0 LLM
+    """
+
+    def __init__(self, model_path: str) -> None:
+        print("Creat tokenizer...")
+        self.tokenizer = AutoTokenizer.from_pretrained(model_path, add_eos_token=False, add_bos_token=False, eos_token='<eod>')
+        self.tokenizer.add_tokens(['<sep>', '<pad>', '<mask>', '<predict>', '<FIM_SUFFIX>', '<FIM_PREFIX>', '<FIM_MIDDLE>','<commit_before>','<commit_msg>','<commit_after>','<jupyter_start>','<jupyter_text>','<jupyter_code>','<jupyter_output>','<empty_output>'], special_tokens=True)
+
+        print("Creat model...")
+        self.model = AutoModelForCausalLM.from_pretrained(model_path, torch_dtype=torch.bfloat16, trust_remote_code=True).cuda()
+
+        print(f'Loading Yuan2.0 model from {model_path}.')
+
+    def generate(self, question: str, context: List):
+        if context:
+            prompt = f'背景：{context}\n问题：{question}\n请基于背景，回答问题。'
+        else:
+            prompt = question
+
+        prompt += "<sep>"
+        inputs = self.tokenizer(prompt, return_tensors="pt")["input_ids"].cuda()
+        outputs = self.model.generate(inputs, do_sample=False, max_length=1024)
+        output = self.tokenizer.decode(outputs[0])
+
+        print(output.split("<sep>")[-1])
+print("> Create Yuan2.0 LLM...")
+model_path = './IEITYuan/Yuan2-2B-Mars-hf'
+llm = LLM(model_path)
+print('> Without RAG:')
+llm.generate(question, [])
+
+print('> With RAG:')
+llm.generate(question, context)
